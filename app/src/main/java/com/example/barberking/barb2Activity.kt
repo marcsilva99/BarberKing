f7405ff10740469ea60cd170ace1bb2956ccdabc
andre <santos.andre1999@hotmail.com>
2021-11-23 17:58:18 +0000

    ola

---
+++ app/src/main/java/com/example/barberking/barb2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.barberking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class barb2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_barb2)
+    }
+}
