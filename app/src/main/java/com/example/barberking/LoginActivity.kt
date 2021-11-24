6c7760e5e0480b24454f40dae8248544de8c3de9
Marcelo Silva <marcelosilva-99@hotmail.com>
2021-11-24 00:23:16 +0000

    update

--- app/src/main/java/com/example/barberking/LoginActivity.kt
+++ app/src/main/java/com/example/barberking/LoginActivity.kt
@@ -1,9 +1,10 @@
 package com.example.barberking
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.TextView
 
 
 class LoginActivity : AppCompatActivity() {
@@ -12,6 +13,18 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loginn)
+
+        val register = findViewById(R.id.tvRegister) as TextView
+        register.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        val login = findViewById(R.id.btLogin) as TextView
+        login.setOnClickListener {
+            val intent = Intent(this, MainnActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
--- app/src/main/java/com/example/barberking/RegisterActivity.kt
+++ app/src/main/java/com/example/barberking/RegisterActivity.kt
@@ -1,12 +1,20 @@
 package com.example.barberking
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registerr)
+
+        val registar2 = findViewById(R.id.btRegister) as TextView
+        registar2.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
