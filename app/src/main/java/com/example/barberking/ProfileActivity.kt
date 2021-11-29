86cc7db745981b3ad29a858dd2328d350ed502bd
Marcelo Silva <marcelosilva-99@hotmail.com>
2021-11-29 19:47:07 +0000

    update

--- app/src/main/java/com/example/barberking/ProfileActivity.kt
+++ app/src/main/java/com/example/barberking/ProfileActivity.kt
@@ -1,6 +1,8 @@
 package com.example.barberking
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ProfileActivity : AppCompatActivity() {
@@ -8,5 +10,12 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile);
+
+        val login2 = findViewById(R.id.btLogout) as TextView
+        login2.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
