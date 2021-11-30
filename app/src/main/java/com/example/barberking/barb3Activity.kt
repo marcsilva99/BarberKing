0eca2b441d181b7d08f304c4bc1e22193031ca7c
andre <santos.andre1999@hotmail.com>
2021-11-30 01:33:27 +0000

    mudanças curtas

--- app/src/main/java/com/example/barberking/barb3Activity.kt
+++ app/src/main/java/com/example/barberking/barb3Activity.kt
@@ -1,11 +1,20 @@
 package com.example.barberking
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class barb3Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_barb3)
+
+        val ButtonBarb3 = findViewById(R.id.tvButtonBarb3) as TextView
+        ButtonBarb3.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/maps/dir//Barbearia+24,+R.+Com%C3%A9rcio+838/@40.9885723,-8.4844375,937m/data=!3m1!1e3!4m9!4m8!1m0!1m5!1m1!1s0xd248177b85e76b3:0x73865f80272c52a!2m2!1d-8.4840567!2d40.9884143!3e0?hl=pt-PT"))
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/barberking/barb4Activity.kt
+++ app/src/main/java/com/example/barberking/barb4Activity.kt
@@ -1,12 +1,21 @@
 package com.example.barberking
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 
 class barb4Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_barb4)
+
+        val ButtonBarb4 = findViewById(R.id.tvButtonBarb4) as TextView
+        ButtonBarb4.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/maps/dir//Daniel+Reis+O+Barbeiro,+R.+de+Soares+dos+Reis+191,+4430-315+Vila+Nova+de+Gaia/@41.1215403,-8.6107356,936m/data=!3m2!1e3!5s0xd2464cd809496a3:0x84b3417553dc28f3!4m9!4m8!1m0!1m5!1m1!1s0xd2464d47ed9699b:0x3d763c0d7e40821f!2m2!1d-8.6101869!2d41.1213426!3e0?hl=pt-PT"))
+            startActivity(intent)
+        }
     }
 }
