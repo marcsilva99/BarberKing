9e17afba5787ba49c3382528137f22e00b99781d
andre <santos.andre1999@hotmail.com>
2021-11-30 16:11:40 +0000

    mudanças curtas

---
+++ app/src/main/java/com/example/barberking/TipoCorte.kt
@@ -0,0 +1,11 @@
+package com.example.barberking
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TipoCorte : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tipo_corte)
+    }
+}
