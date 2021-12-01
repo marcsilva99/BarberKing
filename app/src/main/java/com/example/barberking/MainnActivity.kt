ba8da5bd01171a14f8611fd02f0541a2524810d5
Marcelo Silva <marcelosilva-99@hotmail.com>
2021-12-01 18:50:05 +0000

    update

--- app/src/main/java/com/example/barberking/MainnActivity.kt
+++ app/src/main/java/com/example/barberking/MainnActivity.kt
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 
 class MainnActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mainn);
@@ -47,4 +48,5 @@ class MainnActivity : AppCompatActivity() {
         }
 
     }
+
 }
--- app/src/main/java/com/example/barberking/TipoCorteActivity.kt
+++ app/src/main/java/com/example/barberking/TipoCorteActivity.kt
@@ -3,7 +3,7 @@ package com.example.barberking
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class TipoCorte : AppCompatActivity() {
+class TipoCorteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tipo_corte)
