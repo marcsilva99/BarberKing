6b3479bd552c4b05078bb12e5b1fc431dd0ca4fc
andre <santos.andre1999@hotmail.com>
2021-12-29 03:17:59 +0000

    pagina inicial atualizada

--- app/src/main/java/com/example/barberking/MainnActivity.kt
+++ app/src/main/java/com/example/barberking/MainnActivity.kt
@@ -11,40 +11,20 @@ class MainnActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mainn);
 
-        val image_view = findViewById(R.id.imageView1) as ImageView
-        image_view.setOnClickListener {
-            val intent = Intent(this, barb1Activity::class.java)
-            startActivity(intent)
-        }
-
-        val image_view2 = findViewById(R.id.imageView2) as ImageView
-        image_view2.setOnClickListener {
-            val intent = Intent(this, barb2Activity::class.java)
-            startActivity(intent)
-        }
-        val image_view3 = findViewById(R.id.imageView3) as ImageView
-        image_view3.setOnClickListener {
-            val intent = Intent(this, barb3Activity::class.java)
-            startActivity(intent)
-        }
-
-        val image_view4 = findViewById(R.id.imageView4) as ImageView
-        image_view4.setOnClickListener {
-            val intent = Intent(this, barb4Activity::class.java)
-            startActivity(intent)
-        }
-
-        val login = findViewById(R.id.tvloginMain) as TextView
+        val login = findViewById(R.id.loginimg) as ImageView
         login.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
 
-        val profile = findViewById(R.id.tvlojas) as TextView
-        profile.setOnClickListener {
-            val intent = Intent(this, ProfileActivity::class.java)
+        val barbearias = findViewById(R.id.barbeariasimg) as ImageView
+        barbearias.setOnClickListener {
+            val intent = Intent(this, BarbeariasActivity::class.java)
             startActivity(intent)
         }
+
+
+
 
     }
 }
