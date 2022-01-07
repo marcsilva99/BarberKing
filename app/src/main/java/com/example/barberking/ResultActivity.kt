1d0b4fe5a5c45d0b3652feceb7ae68f942bf4e50
andre <santos.andre1999@hotmail.com>
2022-01-07 14:42:46 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/example/barberking/ResultActivity.kt
+++ app/src/main/java/com/example/barberking/ResultActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_result.*
 
@@ -24,13 +25,18 @@ class ResultActivity : AppCompatActivity() {
             premio()
         }
 
+        val fazerparabens = findViewById(R.id.tv_congratulations) as TextView
+        fazerparabens.let {
+            parabens()
+        }
+
         val userName = intent.getStringExtra(Constants.USER_NAME)
-        tv_name.text = userName
+
 
         val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
         val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
 
-        tv_score.text = "Your Score is $correctAnswers out of $totalQuestions."
+        tv_score.text = "Acertaste $correctAnswers de $totalQuestions Perguntas"
 
         btn_finish.setOnClickListener {
             val intent = Intent(this@ResultActivity, MainnActivity::class.java)
@@ -40,6 +46,20 @@ class ResultActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    private fun parabens() {
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
+        if (correctAnswers > 9) {
+            val parabens1 = findViewById(R.id.tv_congratulations) as TextView
+            parabens1.text="Parabéns conseguiste ganhar o vale"
+        } else {
+            val parabens2 = findViewById(R.id.tv_congratulations) as TextView
+            parabens2.text="Foi por pouco, não conseguiste ganhar o vale"
+        }
+
+    }
+
+
 
 
     private fun premio() {
