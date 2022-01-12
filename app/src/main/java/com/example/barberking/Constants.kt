bd9f24be43eaec8ccc8392395d3843dbb8dfe000
andre <santos.andre1999@hotmail.com>
2022-01-12 11:11:03 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/example/barberking/Constants.kt
+++ app/src/main/java/com/example/barberking/Constants.kt
@@ -14,100 +14,100 @@ object Constants {
 
         // 1
         val que1 = Question(
-            1, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_argentina,
-            "Argentina", "Australia",
-            "Armenia", "Austria", 1
+            1, "Qual o livro mais vendido do mundo?",
+            R.drawable.ic_livro,
+            "Dom Quixote", "O Código Da Vinci",
+            "O Diária de Anne Frank", "Biblia", 4
         )
 
         questionsList.add(que1)
 
         // 2
         val que2 = Question(
-            2, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_australia,
-            "Angola", "Austria",
-            "Australia", "Armenia", 3
+            2, "Quem foi responsável por esta pintura no teto da capela sistina?",
+            R.drawable.ic_capela,
+            "Vincent Van Gogh", "Pablo Picasso",
+            "Miguel Ângelo", "Salvador Dalí", 3
         )
 
         questionsList.add(que2)
 
         // 3
         val que3 = Question(
-            3, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_brazil,
-            "Belarus", "Belize",
-            "Brunei", "Brazil", 4
+            3, "Qual o planeta localizado entre Marte e Saturno no sistema solar?",
+            R.drawable.ic_solar,
+            "Terra", "Neptuno",
+            "Venus", "Jupiter", 4
         )
 
         questionsList.add(que3)
 
         // 4
         val que4 = Question(
-            4, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_belgium,
-            "Bahamas", "Belgium",
-            "Barbados", "Belize", 2
+            4, "A França tem fronteira com qual do conjunto de países?",
+            R.drawable.ic_franca,
+            "Belgica, Luxemburgo, Alemanha, Suíça, Itália, Espanho e Androrra", "Belgica, Suiça, Espanha, Alemanha, Austria e Andorra",
+            "Belgica, Suiça, Espanha e Andorra", "Belgica, Espanha, Andorra e Alemanha", 1
         )
 
         questionsList.add(que4)
 
         // 5
         val que5 = Question(
-            5, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_fiji,
-            "Gabon", "France",
-            "Fiji", "Finland", 3
+            5, "Enquanto Profissional, quantos clubes representou Cristiano Ronaldo",
+            R.drawable.ic_cr7,
+            "4", "5",
+            "3", "6", 1
         )
 
         questionsList.add(que5)
 
         // 6
         val que6 = Question(
-            6, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_germany,
-            "Germany", "Georgia",
-            "Greece", "none of these", 1
+            6, "Qual das musicas não é da banda Queen",
+            R.drawable.ic_queen,
+            "Save Me", "I Want To Break Free",
+            "Bohemian Rhapsody", "Another Brick in the Wall", 4
         )
 
         questionsList.add(que6)
 
         // 7
         val que7 = Question(
-            7, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_denmark,
-            "Dominica", "Egypt",
-            "Denmark", "Ethiopia", 3
+            7, "Quem foi o ultimo português/a a conquista uma medalha de ouro olimpica",
+            R.drawable.ic_olimpicos,
+            "Patricia Mamoa", "Nelson Évora",
+            "Pedro Pichardo", "Fernando Pimenta", 3
         )
 
         questionsList.add(que7)
 
         // 8
         val que8 = Question(
-            8, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_india,
-            "Ireland", "Iran",
-            "Hungary", "India", 4
+            8, "Em qual estação do ano a barba tem tendencia para crescer mais?",
+            R.drawable.ic_barba,
+            "Inverno", "Outono",
+            "Primavera", "Verão", 4
         )
 
         questionsList.add(que8)
 
         // 9
         val que9 = Question(
-            9, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_new_zealand,
-            "Australia", "New Zealand",
-            "Tuvalu", "United States of America", 2
+            9, "Qual o local que todos os anos entra em primeiro lugar no ano novo?",
+            R.drawable.ic_fogo,
+            "Australia", "Kiritimati",
+            "Nova Zelandia", "Nauru", 2
         )
 
         questionsList.add(que9)
 
         // 10
         val que10 = Question(
-            10, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_kuwait,
-            "Kuwait", "Jordan",
-            "Sudan", "Palestine", 1
+            10, "Quantos elementos químicos estão representados na atual tabela periódica",
+            R.drawable.ic_tabela,
+            "112", "118",
+            "80", "120", 2
         )
 
         questionsList.add(que10)
