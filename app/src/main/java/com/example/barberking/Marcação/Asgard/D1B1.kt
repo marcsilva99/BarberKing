853db388eba1959c8f299415c82cb6b1912da172
Marcelo <marcelosilva-99@hotmail.com>
2022-02-18 13:12:39 +0000

    teste

--- app/src/main/java/com/example/barberking/Marcação/Asgard/D1B1.kt
+++ app/src/main/java/com/example/barberking/Marcação/Asgard/D1B1.kt
@@ -14,23 +14,28 @@ import androidx.core.content.ContextCompat
 import com.example.barberking.R
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.activity_barbeiro1.*
+import com.google.firebase.firestore.auth.User
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_d1_b1.*
-import kotlinx.android.synthetic.main.activity_jogo.*
 
-
-class Barbeiro1Activity : AppCompatActivity() {
+class D1B1 : AppCompatActivity() {
+    private val dbFirestore: DocumentReference = FirebaseFirestore.getInstance().collection("Agendamento")
+        .document("Asgard").collection("Barbeiro1").document("Fevereiro").collection("Dias").document("1")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_barbeiro1)
+        setContentView(R.layout.activity_d1_b1)
 
 
-        val D1 = findViewById(R.id.D1) as TextView
-        D1.setOnClickListener {
-            val intent = Intent(this, D1B1::class.java)
-            startActivity(intent)
+        h1011.setOnClickListener {
+            h1011.setTextColor(
+                Color.parseColor("#363A43")
+            )
+            h1011.setTypeface(h1011.typeface, Typeface.BOLD)
+            h1011.background = ContextCompat.getDrawable(
+                this@D1B1,
+                R.drawable.selected_option_border_bg
+            )
         }
-
     }
 }
