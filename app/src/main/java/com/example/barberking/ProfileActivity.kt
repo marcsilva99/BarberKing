a77a7928666540e627d3c497edfb797553e49eec
Marcelo <marcelosilva-99@hotmail.com>
2022-02-22 17:27:49 +0000

    teste

--- app/src/main/java/com/example/barberking/ProfileActivity.kt
+++ app/src/main/java/com/example/barberking/ProfileActivity.kt
@@ -2,12 +2,17 @@ package com.example.barberking
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_loginn.*
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -28,8 +33,33 @@ class ProfileActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val email3 : TextView = findViewById(R.id.email)
+        email3.let{
+            perfil()
+        }
+
+//        showHideBtn.setOnClickListener {
+//            if(showHideBtn.text.toString().equals("Show")){
+//                pwd.transformationMethod = HideReturnsTransformationMethod.getInstance()
+//                showHideBtn.text = "Hide"
+//            } else{
+//                pwd.transformationMethod = PasswordTransformationMethod.getInstance()
+//                showHideBtn.text = "Show"
+//            }
+//        }
+
     }
     private fun logout(){
         Firebase.auth.signOut()
+    }
+
+    private fun perfil() {
+        val user = Firebase.auth.currentUser
+        user?.let {
+            val email2 = user.email
+            email.text = email2
+//            val pass2 = user.
+//            pwd.text = pass2.toString()
+        }
     }
 }
